5a31b604b75a4f41c43a6db4d92abf369a8899a2
aab <latierramedia@gmail.com>
2025-03-10 13:00:36 +0100

    añadido base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://andune55.github.io/notas-fos/",
   plugins: [
     tailwindcss(),
     react()
